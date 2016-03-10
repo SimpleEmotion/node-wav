8553a7d1ca9a5252784b27ebff4281c97f81d683
Akash Krishnan <ak@aakay.net>
2016-03-10 18:19:28 -0500

    expanded File.hash mocha test cases

--- test/file.js
+++ test/file.js
@@ -7,11 +7,44 @@ describe( 'File', function () {
 
   describe( '#hash()', function () {
 
-    it( 'should calculate correct hash for T2a6uvl5c4m7ozqnt37wejkt32a.wav', function ( done ) {
+    it( 'should throw an error for nonexistent file', function ( done ) {
 
+      var filename = path.resolve( __dirname, './audio/nonexistent-file.wav' );
+
+      Wav.file.hash( filename, function ( err ) {
+
+        if ( err ) {
+          return done();
+        }
+
+        done( new Error( 'Expected error.' ) );
+
+      } );
+
+    } );
+
+    it( 'should throw an error for invalid wav file', function ( done ) {
+
+      var filename = path.resolve( __dirname, './file.js' );
+
+      Wav.file.hash( filename, function ( err ) {
+
+        if ( err ) {
+          return done();
+        }
+
+        done( new Error( 'Expected error.' ) );
+
+      } );
+
+    } );
+
+    it( 'should calculate correct hash for file T2a6uvl5c4m7ozqnt37wejkt32a.wav', function ( done ) {
+
+      var filename = path.resolve( __dirname, './audio/T2a6uvl5c4m7ozqnt37wejkt32a.wav' );
       var expected = 'AC7A05D0EE017731F5BDA0CAA45A8A553AE3A8CA828694932C9EB2A49BD4ADC3';
 
-      Wav.file.hash( path.resolve( __dirname, './audio/T2a6uvl5c4m7ozqnt37wejkt32a.wav' ), function ( err, hash ) {
+      Wav.file.hash( filename, function ( err, hash ) {
 
         if ( err ) {
           return done( err );
@@ -28,11 +61,12 @@ describe( 'File', function () {
 
     } );
 
-    it( 'should calculate correct hash for T2aaoxo63yfkykfmdr7yehuzqza.wav', function ( done ) {
+    it( 'should calculate correct hash for file T2aaoxo63yfkykfmdr7yehuzqza.wav', function ( done ) {
 
+      var filename = path.resolve( __dirname, './audio/T2aaoxo63yfkykfmdr7yehuzqza.wav' );
       var expected = '118DA6568EE44D31A7335185563866A447C6B3CC76FFAB181CC00E4EF4BD5AC0';
 
-      Wav.file.hash( path.resolve( __dirname, './audio/T2aaoxo63yfkykfmdr7yehuzqza.wav' ), function ( err, hash ) {
+      Wav.file.hash( filename, function ( err, hash ) {
 
         if ( err ) {
           return done( err );
