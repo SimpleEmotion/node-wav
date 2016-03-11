960fad0e896f165e8f3526ce935a86372a1527a5
Akash Krishnan <ak@aakay.net>
2016-03-10 21:54:19 -0500

    defined pending mocha tests for Convert and Convert.file

---
+++ test/convert.js
@@ -0,0 +1,171 @@
+'use strict';
+
+var Wav = require( '..' );
+var fs = require( 'fs' );
+var path = require( 'path' );
+
+describe( 'Convert', function () {
+
+  describe( '#file()', function () {
+
+    describe( 'nonexistent file', function () {
+
+      var filename = 'file-that-does-not-exist.wav';
+      var inputFilename = path.resolve( path.join( __dirname, 'audio', filename ) );
+      var outputFilename = inputFilename + '.wav';
+
+      before( 'ensure input file does not exist', function ( done ) {
+        fs.exists( inputFilename, function ( exists ) {
+
+          if ( exists ) {
+            return done( new Error( 'Input file exists.' ) );
+          }
+
+          done();
+
+        } );
+      } );
+
+      before( 'ensure output file does not exist', function ( done ) {
+
+        // Check if output file already exists
+        fs.exists( outputFilename, function ( exists ) {
+
+          if ( !exists ) {
+            return done();
+          }
+
+          // Remove preexisting output file
+          fs.unlink( outputFilename, function ( err ) {
+
+            if ( err ) {
+              return done( err );
+            }
+
+            done();
+
+          } );
+
+        } );
+
+      } );
+
+      it( 'should throw an error' );
+
+      it( 'should not overwrite a file' );
+
+      it( 'should not create a file' );
+
+    } );
+
+    describe( 'invalid file', function () {
+
+      var filename = 'convert.js';
+      var inputFilename = path.resolve( path.join( __dirname, filename ) );
+      var outputFilename = inputFilename + '.wav';
+
+      before( 'ensure input file exists', function ( done ) {
+        fs.exists( inputFilename, function ( exists ) {
+
+          if ( !exists ) {
+            return done( new Error( 'Input file does not exist.' ) );
+          }
+
+          done();
+
+        } );
+      } );
+
+      before( 'ensure output file does not exist', function ( done ) {
+
+        // Check if output file already exists
+        fs.exists( outputFilename, function ( exists ) {
+
+          if ( !exists ) {
+            return done();
+          }
+
+          // Remove preexisting output file
+          fs.unlink( outputFilename, function ( err ) {
+
+            if ( err ) {
+              return done( err );
+            }
+
+            done();
+
+          } );
+
+        } );
+
+      } );
+
+      it( 'should throw an error' );
+
+      it( 'should not overwrite a file' );
+
+      it( 'should not create a file' );
+
+    } );
+
+    describe( 'T2a6uvl5c4m7ozqnt37wejkt32a.wav', function () {
+
+      var filename = 'T2a6uvl5c4m7ozqnt37wejkt32a.wav';
+      var inputFilename = path.resolve( path.join( __dirname, 'audio', filename ) );
+      var outputFilename = inputFilename + '.wav';
+
+      before( 'ensure input file exists', function ( done ) {
+        fs.exists( inputFilename, function ( exists ) {
+
+          if ( !exists ) {
+            return done( new Error( 'Input file does not exist.' ) );
+          }
+
+          done();
+
+        } );
+      } );
+
+      before( 'ensure output file does not exist', function ( done ) {
+
+        // Check if output file already exists
+        fs.exists( outputFilename, function ( exists ) {
+
+          if ( !exists ) {
+            return done();
+          }
+
+          // Remove preexisting output file
+          fs.unlink( outputFilename, function ( err ) {
+
+            if ( err ) {
+              return done( err );
+            }
+
+            done();
+
+          } );
+
+        } );
+
+      } );
+
+      it( 'should create the output file' );
+
+      it( 'should overwrite the output file' );
+
+      it( 'should create a valid output wav file' );
+
+      it( 'should create a correct output wav file' );
+
+    } );
+
+  } );
+
+  describe.skip( '#dir()', function () {
+
+    //
+
+  } );
+
+} );
